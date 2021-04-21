f6be0926edae17b8d9279ddd480af7902f77e304
Mehdi <mehdi.zaoui@esprit.tn>
2021-04-21 22:50:10 +0100

    add

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];

c4b3c5ecad686626688ff6cac6f0bec636d4e146
Dali <mohamedali.omri@esprit.tn>
2021-04-21 21:38:37 -0700

    first commit

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -3,13 +3,26 @@
 namespace App\Controller;
 
 use App\Entity\Commande;
+use App\Entity\Produit;
+use App\Entity\User;
 use App\Form\CommandeType;
+use App\Repository\CommandeRepository;
+use Doctrine\ORM\Mapping\Entity;
+use PHPMailer\PHPMailer\PHPMailer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Console\Tester\CommandTester;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+
+// Include Dompdf required namespaces
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
+//mailing
+//require_once ('C:\Users\Dali\PhpstormProjects\Pidev\vendor\phpmailer');
 
 class CommandeController extends AbstractController
 {
@@ -18,11 +31,9 @@ class CommandeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('commande/shows.html.twig', [
+        return $this->render('template.html.twig', [
             'controller_name' => 'CommandeController',
         ]);
-
-
     }
 
     /**
@@ -39,7 +50,6 @@ class CommandeController extends AbstractController
                 $manager->remove($Commande);
                 $manager->flush();
                 return new JsonResponse(array('operation' => 'succes'));
-
             } else {
                 return new JsonResponse(array('operation' => 'failure'));
             }
@@ -108,11 +118,9 @@ class CommandeController extends AbstractController
             $em = $this->getDoctrine()->getManager();
             $em->persist($commande);
             $em->flush();
-
             return $this->redirect('/view-commande/' . $commande->getId());
 
         }
-
         return $this->render(
             'create.html.twig',
             array('form' => $form->createView())
@@ -155,13 +163,156 @@ class CommandeController extends AbstractController
             'Valider Avec Succès'
         );
         $this->getDoctrine()->getManager()->flush();
+        $this->getDoctrine()->getManager()->flush();
 
+        //mailing
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = 'ssl';
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = '465';
+        $mail->isHTML();
+        $mail->Username = 'mohamedali.omri@esprit.tn';
+        $mail->Password = 'omriiT9141';
+        $mail->SetFrom('no-reply@ignite.tn');
+        $mail->Subject = 'IGNITE service client';
+        $mail->Body = 'Votre Commande est validé';
+        $mail->addAddress('omrixo.dali97@gmail.com');
+        $mail->send();
 
 
         return $this->redirectToRoute('show-commandes');
-
-
-
     }
+
+    /**
+     * @Route("/cart/checkout", name="getCartInfo")
+     */
+    public function renderCart(){
+        return $this->render('commande/cart.html.twig');
+    }
+
+
+    /**
+     * @Route("/create-order", name="createOrderAjax")
+     */
+    public function createOrder(Request $request){
+        // si la requete est de type ajax
+        if ($request->isXmlHttpRequest()){
+
+            $commande=new Commande();
+            $commande->setPrixtotale((float)$request->request->get('total'));
+            $commande->setValide(0);
+            $commande->setDatecom(new \DateTime('now'));
+
+            $user=$this->getDoctrine()->getRepository(user::class)->find(1);
+
+            $commande->setUser($user);
+            $this->getDoctrine()->getManager()->persist($commande);
+            $products=$request->request->get('products');
+            foreach ($products as $single)
+            {
+                $product=$this->getDoctrine()->getRepository(Produit::class)->find((int)$single["id"]);
+
+                $commande->addProduit($product);
+                $this->getDoctrine()->getManager()->flush();
+
+
+            }
+            //mailing
+            $mail = new PHPMailer();
+            $mail->isSMTP();
+            $mail->SMTPAuth = true;
+            $mail->SMTPSecure = 'ssl';
+            $mail->Host = 'smtp.gmail.com';
+            $mail->Port = '465';
+            $mail->isHTML();
+            $mail->Username = 'mohamedali.omri@esprit.tn';
+            $mail->Password = 'omriiT9141';
+            $mail->SetFrom('no-reply@ignite.tn');
+            $mail->Subject = 'IGNITE service client';
+            $mail->Body = 'Votre Commande est bien enregistrer, merci';
+            $mail->addAddress('omrixo.dali97@gmail.com');
+            $mail->send();
+
+            $this->getDoctrine()->getManager()->persist($commande);
+            return new JsonResponse(array('operation'=>'success'));
+        }
+        else return new Response('please use ajax');
+    }
+
+    public function showProduct(){
+
+        $em= $this->getDoctrine()->getManager();
+        $Product =$em->getRepository('App\Entity\Produit')->findAll();
+
+        return $this->render('commande/shop-grid.html.twig',array(
+            'Products'=> $Product));
+    }
+
+    /**
+     * @Route("/create-pdf", name="createPdf")
+     */
+    public function pdf()
+    {
+        //$dompdf -> loadHtml("<img src='images/icon.png'>");
+
+        $commande = $this->getDoctrine()
+            ->getRepository('App\Entity\Commande')
+            ->findAll();
+
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($commande);
+
+
+        $html = $this->renderView('commande/mypdf.html.twig',array(
+            'commandes'=> $commande));
+
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (inline view)
+        $dompdf->stream("mypdf.pdf", [
+            "Attachment" => false
+        ]);
+    }
+    /**
+     * @Route("/create-pdfPanier", name="createPdfPanier")
+     */
+    public function pdfPanier()
+    {
+        //$dompdf -> loadHtml("<img src='images/icon.png'>");
+
+        $commande = $this->render('commande/cart.html.twig');
+
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($commande);
+
+
+        $html = $this->renderView('commande/panierPdf.html.twig',array(
+            'commandes'=> $commande));
+
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (inline view)
+        $dompdf->stream("mypdf.pdf", [
+            "Attachment" => false
+        ]);
+    }
+
+
 
 }
